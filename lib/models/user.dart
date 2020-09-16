bdb977340725b19d41129ff78854fc88bf28e5ce
Arun Singh Lodhi <arun.7k9@gmail.com>
2020-09-16 10:59:41 +0530

    Brew Crew

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+class CustomUser {
+  final String uid;
+  CustomUser({this.uid});
+}
+
+class CustomUserData {
+  final String uid;
+  final String name;
+  final String sugars;
+  final int strength;
+
+  CustomUserData({this.uid, this.sugars, this.strength, this.name});
+}
